1ae789bb9066bdd057dd1ea9b7c78ca2eefdf7f1
Aswathi Prakash <inboxaswathi@gmail.com>
2020-10-14 13:11:56 +1100

    initial project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListPageComponent } from './components/list-page/list-page.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPageComponent,
+    DetailsPageComponent
   ],
   imports: [
     BrowserModule,
